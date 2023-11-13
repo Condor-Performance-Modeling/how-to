0955b6cb3c582f5ec80ce58080e31f331b539d00
Jeff Nye <jeff@www.condorcomputing.com>
2023-11-13 11:36:39 -0600

    scons added to list, checking in build_golden
    scons is required for gem5 builds
    
    build_golden has directory checking and now
    exits conda sparta environment before build

--- scripts/build_golden_models.sh
+++ scripts/build_golden_models.sh
@@ -3,11 +3,41 @@ if [[ -z "${CONDOR_TOP}" ]]; then
   { echo "CONDOR_TOP is undefined, execute 'source how-to/env/setuprc.sh'"; exit 1; }
 fi
 
+if [[ -z "${SPIKE}" ]]; then
+{
+  echo "-E: SPIKE is undefined, execute 'source how-to/env/setuprc.sh'";
+  exit 1;
+}
+fi
+
+if [[ -z "${WHISPER}" ]]; then
+{
+  echo "-E: WHISPER is undefined, execute 'source how-to/env/setuprc.sh'";
+  exit 1;
+}
+fi
+
+if ! [ -d "$SPIKE" ]; then
+{
+  echo "-W: riscv-isa-sim does not exist, cloning repo."
+  git clone git@github.com:riscv/riscv-isa-sim.git
+}
+fi
+
+if ! [ -d "$WHISPER" ]; then
+{
+  echo "-W: whisper does not exist, cloning repo."
+  git clone https://github.com/chipsalliance/VeeR-ISS.git whisper
+}
+fi
+
 mkdir -p $TOOLS/bin
 
+# once for sparta, once for base
+conda deactivate
+conda deactivate
+
 # Spike
-cd $CONDOR_TOP
-git clone git@github.com:riscv/riscv-isa-sim.git
 cd $SPIKE
 mkdir -p build; cd build
 ../configure --prefix=$TOP/tools
@@ -15,8 +45,6 @@ make -j8
 make install
 
 # Whisper
-cd $CONDOR_TOP
-git clone https://github.com/chipsalliance/VeeR-ISS.git whisper
 cd $WHISPER
 make -j8 
 cp build-Linux/whisper $TOOLS/bin
