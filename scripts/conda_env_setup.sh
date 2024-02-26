377c29771bdd93120f0955156b5482d36ea5dc70
Stan Iwan <stan.iwan@sofomo.com>
2024-02-26 15:19:59 -0600

    Add script to set up the conda enviroment

---
+++ scripts/conda_env_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -z "$TOP" ]] || [[ -z "$MAP" ]]; then
+    echo "The TOP or MAP environment variables are not set. Please set them before continuing."
+    exit 1
+fi
+
+if [[ "$CONDA_DEFAULT_ENV" != "base" ]]; then
+    echo "The 'conda' environment is not active. Please activate it before continuing."
+    echo "To activate the 'conda' environment, run: conda activate conda"
+    exit 1
+fi
+
+echo "Installing conda-forge packages..."
+conda install -c conda-forge jq yq -y
+
+echo "Cloning the MAP repository..."
+cd $TOP
+git clone https://github.com/sparcians/map.git
+
+echo "Checking out the map_v2 branch..."
+cd $MAP
+git checkout map_v2
+
+echo "Creating the Sparta Conda environment..."
+./scripts/create_conda_env.sh sparta dev
+
+echo "Setup process completed."
+echo "Please activate the 'sparta' environment by running: conda activate sparta"
