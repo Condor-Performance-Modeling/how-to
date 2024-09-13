e66ece0bb73192e0e156b454486d8d19a5f1ccc2
Stan Iwan <stan.iwan@sofomo.com>
2024-09-13 08:38:54 -0500

    Test build code

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -60,30 +60,45 @@ confirm_proceed() {
     fi
 }
 
-clone_repository() {
-    local repo_url="$1"
-    local destination_dir="$2"
+clone_riscv_gcc_toolchain() {
+    log_step "Cloning RISC-V GCC Toolchain"
+    
+    git clone --recursive https://github.com/riscv/riscv-gnu-toolchain || { echo "Failed to clone RISC-V GNU Toolchain."; exit 1; }
 
-    log_step "Cloning repository into $destination_dir"
+    complete_step "Cloning RISC-V GCC Toolchain"
+}
 
-    if [ -d "$destination_dir" ]; then
-        echo "Directory $destination_dir already exists. Skipping cloning." | tee -a "$LOG_FILE"
-    else
-        git clone "$repo_url" "$destination_dir" || pretty_error "Failed to clone repository."
-        echo "Repository cloned successfully into $destination_dir." | tee -a "$LOG_FILE"
-    fi
-    complete_step "Cloning repository into $destination_dir"
+checkout_gcc_branch() {
+    log_step "Checking out gcc-14 branch in riscv-gnu-toolchain/gcc"
+
+    cd "$SOURCE_DIR/riscv-gnu-toolchain" || pretty_error "Failed to enter riscv-gnu-toolchain directory."
+    cd gcc || pretty_error "Failed to enter gcc subdirectory."
+
+    git fetch --all || pretty_error "Failed to fetch in gcc repository."
+    git checkout origin/releases/gcc-14 || pretty_error "Failed to check out gcc-14 branch."
+
+    complete_step "Checked out gcc-14 branch in gcc"
 }
 
 build_gcc_baremetal() {
     log_step "Building GCC for Baremetal"
     echo "Building GCC for Baremetal in $BAREMETAL_INSTALL_PATH..." | tee -a "$LOG_FILE"
+    cd "$SOURCE_DIR/riscv-gnu-toolchain" || pretty_error "Failed to enter riscv-gnu-toolchain directory."
+
+    ./configure --prefix="$BAREMETAL_INSTALL_PATH" --enable-multilib --with-cmodel=medany || pretty_error "Failed to configure GCC for baremetal."
+    make -j$(nproc) || pretty_error "Failed to build GCC for baremetal."
+
     complete_step "Building GCC for Baremetal"
 }
 
 build_gcc_linux() {
     log_step "Building GCC for Linux"
     echo "Building GCC for Linux in $LINUX_INSTALL_PATH..." | tee -a "$LOG_FILE"
+    cd "$SOURCE_DIR/riscv-gnu-toolchain" || pretty_error "Failed to enter riscv-gnu-toolchain directory."
+
+    ./configure --prefix="$LINUX_INSTALL_PATH" --with-arch=rv64gc --with-abi=lp64d --enable-linux --enable-multilib || pretty_error "Failed to configure GCC for Linux."
+    make linux -j$(nproc) || pretty_error "Failed to build GCC for Linux."
+
     complete_step "Building GCC for Linux"
 }
 
@@ -94,7 +109,8 @@ build_gcc() {
     confirm_proceed
 
     cd "$SOURCE_DIR" || pretty_error "Failed to change to source directory $SOURCE_DIR."
-    clone_repository "git clone git://gcc.gnu.org/git/gcc.git" "gcc"
+    clone_riscv_gcc_toolchain
+    checkout_gcc_branch
     
     build_gcc_baremetal
     build_gcc_linux
