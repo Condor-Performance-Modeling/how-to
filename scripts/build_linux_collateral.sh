b4b5946dd355d2d3acdace26a7b360c795b46012
Stan Iwan <stan.iwan@sofomo.com>
2024-10-24 07:02:33 -0500

    Update build_linux_collateral.sh

--- scripts/build_linux_collateral.sh
+++ scripts/build_linux_collateral.sh
@@ -26,8 +26,10 @@ fi
 wget --no-check-certificate -nc \
         https://git.kernel.org/torvalds/t/linux-5.8-rc4.tar.gz
 tar -xf linux-5.8-rc4.tar.gz
+grep -qxF 'KBUILD_CFLAGS += -march=rv64imafdc_zicsr_zifencei' linux-5.8-rc4/Makefile \
+|| echo 'KBUILD_CFLAGS += -march=rv64imafdc_zicsr_zifencei' >> linux-5.8-rc4/Makefile
 make -C linux-5.8-rc4 ARCH=riscv defconfig
-make -C linux-5.8-rc4 ARCH=riscv -j$(nproc) KBUILD_CFLAGS="-march=rv64imafdc_zicsr_zifencei"
+make -C linux-5.8-rc4 ARCH=riscv -j$(nproc)
 mkdir -p $TOOLS/riscv-linux
 cp linux-5.8-rc4/arch/riscv/boot/Image $TOOLS/riscv-linux/Image
 
